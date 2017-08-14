ec4a1ece935fe2b2bb2042c9822a69c386344622
sujitharajendran <sujikavi66@gmail.com>
2017-08-14 11:12:16 +0530

    Add files via upload

---
+++ Stinglen.java
@@ -0,0 +1,13 @@
+package loc;
+import java.util.*;
+public class Stinglen {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+int d=str.length();
+System.out.println("string length"+d);
+	
+	}
+
+}
